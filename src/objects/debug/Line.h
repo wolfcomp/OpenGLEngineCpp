07fcb8427a2eb3c9bc6246315246b6d2cdd078e8
WildWolf <magnus1997.mho@gmail.com>
2024-08-30 08:21:30 +0200

    upload missing files

---
+++ src/objects/debug/Line.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "../base/Renderable.h"
+
+class Line : public Renderable
+{
+private:
+public:
+    Line() : Renderable({Vertex(), Vertex()}, {0, 1})
+    {
+        set_mode(GL_LINES);
+    };
+    ~Line() {};
+    void set_positions(glm::vec3 start, glm::vec3 end)
+    {
+        std::vector<Vertex> vertices = {
+            Vertex(start),
+            Vertex(end),
+        };
+        update_vertices(vertices);
+    }
+
+    void pre_render() const override
+    {
+        Renderable::pre_render();
+        glLineWidth(5);
+    }
+
+    void post_render() const override
+    {
+        Renderable::post_render();
+        glLineWidth(1);
+    }
+};
