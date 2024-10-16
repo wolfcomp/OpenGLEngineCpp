4eacdb5e3442a2881f8158396bd84f97739d0898
WildWolf <magnus1997.mho@gmail.com>
2024-10-16 12:26:10 +0200

    add las zip documentation implementation

---
+++ SubProjectTesting/LasHeader.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace ConsoleApp3;
+
+[StructLayout(LayoutKind.Sequential, Pack = 1)]
+public unsafe struct LasHeader1_2
+{
+    public fixed byte FileSignature[4];
+    public ushort SourceId;
+    public ushort GlobalEncoding;
+    public fixed byte GuidData[16];
+    public byte VersionMajor;
+    public byte VersionMinor;
+    public fixed byte SystemId[32];
+    public fixed byte GeneratingSoftware[32];
+    public ushort FileCreationDayOfYear;
+    public ushort FileCreationYear;
+    public ushort HeaderSize;
+    public uint OffsetToPointData;
+    public uint NumberOfVariableLengthRecords;
+    public byte PointDataFormatId;
+    public ushort PointDataRecordLength;
+    public uint NumberOfPointRecords;
+    public fixed uint NumberOfPointsByReturn[5];
+    public double XScaleFactor;
+    public double YScaleFactor;
+    public double ZScaleFactor;
+    public double XOffset;
+    public double YOffset;
+    public double ZOffset;
+    public double MaxX;
+    public double MinX;
+    public double MaxY;
+    public double MinY;
+    public double MaxZ;
+    public double MinZ;
+
+    public Guid Guid
+    {
+        get
+        {
+            fixed (byte* ptr = GuidData)
+                return new Guid(new ReadOnlySpan<byte>(ptr, 16));
+        }
+    }
+
+    public string SystemIdString
+    {
+        get
+        {
+            fixed (byte* ptr = SystemId)
+                return Encoding.ASCII.GetString(ptr, 32);
+        }
+    }
+
+    public string GeneratingSoftwareString
+    {
+        get
+        {
+            fixed (byte* ptr = GeneratingSoftware)
+                return new string((sbyte*)ptr);
+        }
+    }
+
+    public override string ToString()
+    {
+        fixed (byte* ptr = FileSignature)
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine("FileSignature: " + Encoding.ASCII.GetString(ptr, 4));
+            sb.AppendLine("SourceId: " + SourceId);
+            sb.AppendLine("GlobalEncoding: " + GlobalEncoding);
+            sb.AppendLine("Guid: " + Guid);
+            sb.AppendLine("VersionMajor: " + VersionMajor);
+            sb.AppendLine("VersionMinor: " + VersionMinor);
+            sb.AppendLine("SystemId: " + SystemIdString);
+            sb.AppendLine("GeneratingSoftware: " + GeneratingSoftwareString);
+            sb.AppendLine("FileCreationDayOfYear: " + FileCreationDayOfYear);
+            sb.AppendLine("FileCreationYear: " + FileCreationYear);
+            sb.AppendLine("HeaderSize: " + HeaderSize);
+            sb.AppendLine("OffsetToPointData: " + OffsetToPointData);
+            sb.AppendLine("NumberOfVariableLengthRecords: " + NumberOfVariableLengthRecords);
+            sb.AppendLine("PointDataFormatId: " + PointDataFormatId);
+            sb.AppendLine("PointDataRecordLength: " + PointDataRecordLength);
+            sb.AppendLine("NumberOfPointRecords: " + NumberOfPointRecords);
+            sb.Append("NumberOfPointsByReturn: [");
+            for (var i = 0; i < 5; i++)
+                sb.Append(NumberOfPointsByReturn[i] + ", ");
+            sb.Remove(sb.Length - 2, 2);
+            sb.AppendLine("]");
+            sb.AppendLine("XScaleFactor: " + XScaleFactor);
+            sb.AppendLine("YScaleFactor: " + YScaleFactor);
+            sb.AppendLine("ZScaleFactor: " + ZScaleFactor);
+            sb.AppendLine("XOffset: " + XOffset);
+            sb.AppendLine("YOffset: " + YOffset);
+            sb.AppendLine("ZOffset: " + ZOffset);
+            sb.AppendLine("MaxX: " + MaxX);
+            sb.AppendLine("MinX: " + MinX);
+            sb.AppendLine("MaxY: " + MaxY);
+            sb.AppendLine("MinY: " + MinY);
+            sb.AppendLine("MaxZ: " + MaxZ);
+            sb.AppendLine("MinZ: " + MinZ);
+            return sb.ToString();
+        }
+    }
+}
---
+++ SubProjectTesting/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+namespace ConsoleApp3;
+
+internal class Program
+{
+    private static unsafe void Main(string[] args)
+    {
+        var path = @"C:\Users\magnu\Downloads\eksport_959441_20241008\774\data\32-05-519-229-56-01.laz";
+        using var fs = new FileStream(path, FileMode.Open, FileAccess.Read);
+        using var br = new BinaryReader(fs);
+        var header = ReadLasHeader(br);
+        var records = new List<LasVariableLengthRecords>();
+        for (var i = 0; i < header.NumberOfVariableLengthRecords; i++)
+        {
+            records.Add(ReadNextRecord(br));
+        }
+
+        var lazRecord = records.FirstOrDefault(t => t is { RecordId: 22204, Reserved: 0xAABB or 0 });
+
+        var lazVLRData = new LazVariableLengthRecordData();
+        fixed (byte* ptr = lazRecord.Data)
+        {
+            lazVLRData.Compressor = (LazVariableLengthRecordData.CompressorType)(*(ushort*)ptr);
+            lazVLRData.Coder = *(ushort*)(ptr + 2);
+            lazVLRData.VersionMajor = *(ptr + 4);
+            lazVLRData.VersionMinor = *(ptr + 5);
+            lazVLRData.VersionRevision = *(ushort*)(ptr + 6);
+            lazVLRData.Options = *(uint*)(ptr + 8);
+            lazVLRData.ChunkSize = *(uint*)(ptr + 12);
+            lazVLRData.NumberOfSpecialEVLR = *(long*)(ptr + 16);
+            lazVLRData.OffsetOfSPecialEVLR = *(long*)(ptr + 24);
+            lazVLRData.NumberOfRecords = *(ushort*)(ptr + 32);
+            lazVLRData.Data = new LazVariableLengthRecordData.LazItemRecord[lazVLRData.NumberOfRecords];
+            var lazVLRItemDataPtr = (LazVariableLengthRecordData.LazItemRecord*)(ptr + 34);
+            for (var i = 0; i < lazVLRData.NumberOfRecords; i++)
+            {
+                lazVLRData.Data[i] = lazVLRItemDataPtr[i];
+            }
+        }
+
+        Console.WriteLine(lazVLRData);
+
+        // Read first item
+        fs.Seek(header.OffsetToPointData, SeekOrigin.Begin);
+
+        var bytes = br.ReadBytes(sizeof(Point10));
+
+        var pointHandle = GCHandle.Alloc(bytes, GCHandleType.Pinned);
+        var point = (Point10)Marshal.PtrToStructure(pointHandle.AddrOfPinnedObject(), typeof(Point10))!;
+        pointHandle.Free();
+
+        var gpsTime = br.ReadDouble();
+
+        Console.WriteLine(point);
+        Console.WriteLine($"GPS Time: {gpsTime}");
+        Console.WriteLine($"Reader position: {fs.Position:X}");
+    }
+
+    public static LasHeader1_2 ReadLasHeader(string path)
+    {
+        using var fs = new FileStream(path, FileMode.Open, FileAccess.Read);
+        using var br = new BinaryReader(fs);
+
+        return ReadLasHeader(br);
+    }
+
+    public static LasHeader1_2 ReadLasHeader(BinaryReader reader)
+    {
+        var headerSize = Marshal.SizeOf<LasHeader1_2>();
+        var headerBytes = reader.ReadBytes(headerSize);
+
+        var headerHandle = GCHandle.Alloc(headerBytes, GCHandleType.Pinned);
+        var header = (LasHeader1_2) Marshal.PtrToStructure(headerHandle.AddrOfPinnedObject(), typeof(LasHeader1_2))!;
+        headerHandle.Free();
+
+        return header;
+    }
+
+    public static unsafe LasVariableLengthRecords ReadNextRecord(BinaryReader reader)
+    {
+        var record = new LasVariableLengthRecords();
+        var recordBytes = reader.ReadBytes(54);
+        fixed (byte* ptr = recordBytes)
+        {
+            record.Reserved = *(ushort*)ptr;
+            record.UserIdString = new string((sbyte*)(ptr + 2));
+            record.RecordId = *(ushort*)(ptr + 18);
+            record.RecordLengthAfterHeader = *(ushort*)(ptr + 20);
+            record.DescriptionString = new string((sbyte*)(ptr + 22));
+        }
+
+        var dataBytes = reader.ReadBytes(record.RecordLengthAfterHeader);
+        record.Data = dataBytes;
+        return record;
+    }
+}
+
